34b14dd71c97e018b9530272ff83435c302f8bcb
Stella <aniretake0910@gmail.com>
2025-12-21 21:41:14 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -10,9 +10,9 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, blank=True)
     previous_evolution = models.ForeignKey(
         'self',
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True)
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True)
 
     def __str__(self):
         return f'{self.title}'
